b22a9a31eb2fc6a617b66fc2bd71f72015a03fd0
Efka97 <ejozokas@gmail.com>
2021-03-22 09:04:27 +0200

    feat: connection to Mongo Compass

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-require('dotenv').config({path: 'server/.env'});
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'server/.env' });
 const carsRouter = require('./routes/cars');
 
 const server = express();
-const { SERVER_PORT } = process.env;
+const {
+  SERVER_PORT,
+  MONGO_COMPASS_USER,
+  MONGO_COMPASS_PASS,
+  MONGO_COMPASS_CLUSTER_URL,
+  MONGO_COMPASS_CLUSTER_DB
+} = process.env;
+
 
 // Middwares
 server.use(cors());
@@ -19,6 +27,15 @@ server.get('/', (req, res) => {
   res.send('Serveris veikia');
 });
 
-server.listen(SERVER_PORT, () => {
-  console.log(`Cars MERN server is running on http://localhost:${SERVER_PORT}`);
+
+const db = mongoose.connection;
+mongoose.connect(`mongodb+srv://${MONGO_COMPASS_USER}:${MONGO_COMPASS_PASS}@${MONGO_COMPASS_CLUSTER_URL}/${MONGO_COMPASS_CLUSTER_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+
+db.on('connected', () => {
+  console.log('Connected to Database successfully!');
+  server.listen(SERVER_PORT, () => {
+    console.log(`Cars MERN server is running on http://localhost:${SERVER_PORT}`);
+  });
 });
+
+db.on('error', (error) => console.error('ERROR: FAILED TO CONNECT TO DB:\n ' + error))
