a578426dfda27ff71f1ad3981319c8b879a549c7
Efka97 <ejozokas@gmail.com>
2021-03-22 09:56:06 +0200

    feat: getCars endpoint

--- server/controllers/cars.js
+++ server/controllers/cars.js
@@ -1,8 +1,13 @@
 const CarModel = require('../models/CarModel');
 
 const getCars = async (req, res) => {
-  const cars = await CarModel.find();
-  res.status(200).json('Ateityje grąžinsiu automobilius')
+  try {
+    const carsFromDB = await CarModel.find();
+    res.status(200).json({ cars: carsFromDB })
+  }
+  catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
 
 const postCar = async (req, res) => {
